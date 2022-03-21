0ed35e1455c24eb6975858aed03d1b7b20d4ddfa
Daniel Schreurs <daniel.schreurs@hotmail.com>
2022-03-21 20:04:51 +0100

    :tada: Initial commit

---
+++ src/js/settings.ts
@@ -0,0 +1,11 @@
+export const settings = {
+    sky: {
+        gradient: ["hsl(219,93%,11%)", "hsl(219,100%,19%)"]
+    },
+    snowflake: {
+        radius: {min: 2, max: 12},
+        opacity: {min: 0.5, max: 0.9},
+        speedX: {min: 1, max: 4},
+        speedY: {min: 1, max: 6},
+    }
+}
